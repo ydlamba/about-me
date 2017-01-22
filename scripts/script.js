44949a1318b26c66bf9c5f5672074365dafc0b3e
Yash Dev Lamba <yashdev18@gmail.com>
2017-01-22 19:30:21 +0530

    all section layout added and footer too

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,46 @@ $(document).ready(function(){
 var $contact = $(".cont");
 
 
-$contact.click(function(){
-
+function change(){
 	$("main").hide();
-	$(".contact").fadeIn(500);
-	//$(".contact").fadeIn(500);
 	$(".backImg").css("transform","scale(1.1)");
-	$(".backImg").css("filter","blur(20px)");
+	$(".backImg").css("filter","blur(10px)");
+}
+$contact.click(function(){
+	change();
+	$(".contact").fadeIn(500);
 	});
+
+
+$(".cross").click(function(){
+
+	$("main").fadeIn(500)
+	$(".contact,.skills,.interests,.education").hide();
+	$(".backImg").css("transform","scale(1.3)");
+	$(".backImg").css("filter","blur(0px)");
+	});
+
+$(".ski").click(function(){
+	change();
+	$(".skills").fadeIn(500);
+});
+$(".inte").click(function(){
+	change();
+	$(".interests").fadeIn(500);
+});
+
+$(".edu").click(function(){
+	change();
+	$(".education").fadeIn(500);
+});
+
+
+
+
+
+
+
+
+
+
 });
